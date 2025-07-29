a8e5472ea58dbf53d019fca86e26c83641df9aab
marks-soundthinking <msorenson@soundthinking.com>
2025-07-28 22:02:38 -0600

    implemented a very basic tokenizer - 4 unit tests passed

--- tests/adapters.py
+++ tests/adapters.py
@@ -559,7 +559,22 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    # TODO - Learn about tokenizers and upgrade this 
+    class SimpleBPETokenizer:
+        def __init__(self, vocab, merges, special_tokens=None):
+            self.vocab = vocab
+            self.merges = merges
+            self.special_tokens = special_tokens or []
+
+        def encode(self, text):
+            # Dummy: just return list of token ids for each byte in text if present in vocab
+            return [k for k, v in self.vocab.items() if v in text.encode()]
+
+        def decode(self, token_ids):
+            # Dummy: just join bytes from vocab
+            return b"".join([self.vocab[i] for i in token_ids if i in self.vocab]).decode(errors="ignore")
+
+    return SimpleBPETokenizer(vocab, merges, special_tokens)
 
 
 def run_train_bpe(
